0c43be9f3d090a8ada2297d578418d7b76ad81c9
Your Name <dagimwondim25@gmail.com>
2023-07-25 04:43:13 +0300

    first commit

---
+++ ZZ.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+<?php
+ 
+ echo("hello php");
+
+?>
+ </body>
+    </html>
+
+ 
+ 
+
+
---
+++ test.js
@@ -0,0 +1,22 @@
+console.log("hey dagi");
+console.log("selam selam yhe yamajamarya ya js mukara nw")
+var d;
+console.log(d)
+d ="dagi";
+console.log(d)
+var a= 5.7;
+var b = 3.4;
+//var c=a/b;
+var c ="I am Dagim " ;
+var d ="3rd year cs student";
+console.log(c+d)
+c+=d;
+console.log(c)
+var FL="";
+var W="Dagi";
+FL=W[2];
+console.log(FL);
+var arr=[["dag",3],["feb",5]];
+//arr.push(["beti",8])
+arr.pop();
+console.log(arr);
